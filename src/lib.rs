0d636a44caa6ee25be0a83b9f90ff3c2bc9daa88
Charlie Saunders <charlieasaunders@gmail.com>
2018-08-17 23:31:39 -0400

    Moves flattening tests to flattening module. Renames flattening function.

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ mod report_to_flat;
 pub mod types;
 
 use failure::Error;
-use report_to_flat::get_flattened;
+use report_to_flat::report_to_flat;
 use types::{Report, ReportResponse};
 
 pub fn to_delimited(raw_report: &String, delimiter: &str) -> Result<String, Error> {
@@ -36,7 +36,7 @@ pub fn to_delimited(raw_report: &String, delimiter: &str) -> Result<String, Erro
         return empty_response;
     }
 
-    Ok(get_flattened(&report, &delimiter))
+    Ok(report_to_flat(&report, &delimiter))
 }
 
 #[cfg(test)]
@@ -81,59 +81,5 @@ mod tests {
         ))).unwrap();
 
         assert_eq!(to_delimited(&data, ",").unwrap(), "".to_string())
-    }
-
-    #[test]
-    fn no_dimensions() {
-        let data: String = fs::read_to_string(PathBuf::from(format!(
-            "{}{}",
-            env!("CARGO_MANIFEST_DIR"),
-            "/test_reports/no_dimensions.json"
-        ))).unwrap();
-
-        assert_eq!(
-            to_delimited(&data, ",").unwrap(),
-            "\"ga:sessions\"\n44\n".to_string()
-        )
-    }
-
-    #[test]
-    fn single_dimension_and_metric() {
-        let data: String = fs::read_to_string(PathBuf::from(format!(
-            "{}{}",
-            env!("CARGO_MANIFEST_DIR"),
-            "/test_reports/single_dimension_and_metric.json"
-        ))).unwrap();
-
-        assert_eq!(
-            to_delimited(&data, "|delimiter|").unwrap(),
-            indoc!(
-                r#""ga:deviceCategory"|delimiter|"ga:sessions"
-                "desktop"|delimiter|43
-                "mobile"|delimiter|1
-                "#
-            ).to_string()
-        )
-    }
-
-    #[test]
-    fn multiple_dimensions_and_metrics() {
-        let data: String = fs::read_to_string(PathBuf::from(format!(
-            "{}{}",
-            env!("CARGO_MANIFEST_DIR"),
-            "/test_reports/multiple_dimensions_and_metrics.json"
-        ))).unwrap();
-
-        assert_eq!(
-            to_delimited(&data, ",").unwrap(),
-            indoc!(
-                r#""ga:deviceCategory","ga:country","ga:sessions","ga:bounces"
-                "desktop","Australia",1,1
-                "desktop","France",39,21
-                "desktop","United States",3,1
-                "mobile","Brazil",1,0
-                "#
-            ).to_string()
-        )
     }
 }
--- src/report_to_flat.rs
+++ src/report_to_flat.rs
@@ -1,7 +1,7 @@
 use joinery::Joinable;
 use types::*;
 
-pub fn get_flattened(report: &Report, delimiter: &str) -> String {
+pub fn report_to_flat(report: &Report, delimiter: &str) -> String {
     let empty_vec = Vec::with_capacity(0);
     let dimension_header_iter = report.column_header.dimensions
         .as_ref()
@@ -47,4 +47,73 @@ pub fn get_flattened(report: &Report, delimiter: &str) -> String {
     }
 
     result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::report_to_flat;
+    use std::fs;
+    use std::path::PathBuf;
+    use serde_json;
+    use types::ReportResponse;
+
+    #[test]
+    fn no_dimensions() {
+        let data: String = fs::read_to_string(PathBuf::from(format!(
+            "{}{}",
+            env!("CARGO_MANIFEST_DIR"),
+            "/test_reports/no_dimensions.json"
+        ))).unwrap();
+
+        let parsed_report: ReportResponse = serde_json::from_str(data.as_str()).unwrap();
+
+        assert_eq!(
+            report_to_flat(&parsed_report.reports[0], ","),
+            "\"ga:sessions\"\n44\n".to_string()
+        )
+    }
+
+    #[test]
+    fn single_dimension_and_metric() {
+        let data: String = fs::read_to_string(PathBuf::from(format!(
+            "{}{}",
+            env!("CARGO_MANIFEST_DIR"),
+            "/test_reports/single_dimension_and_metric.json"
+        ))).unwrap();
+
+        let parsed_report: ReportResponse = serde_json::from_str(data.as_str()).unwrap();
+
+        assert_eq!(
+            report_to_flat(&parsed_report.reports[0], "|delimiter|"),
+            indoc!(
+                r#""ga:deviceCategory"|delimiter|"ga:sessions"
+                "desktop"|delimiter|43
+                "mobile"|delimiter|1
+                "#
+            ).to_string()
+        )
+    }
+
+    #[test]
+    fn multiple_dimensions_and_metrics() {
+        let data: String = fs::read_to_string(PathBuf::from(format!(
+            "{}{}",
+            env!("CARGO_MANIFEST_DIR"),
+            "/test_reports/multiple_dimensions_and_metrics.json"
+        ))).unwrap();
+
+        let parsed_report: ReportResponse = serde_json::from_str(data.as_str()).unwrap();
+
+        assert_eq!(
+            report_to_flat(&parsed_report.reports[0], ","),
+            indoc!(
+                r#""ga:deviceCategory","ga:country","ga:sessions","ga:bounces"
+                "desktop","Australia",1,1
+                "desktop","France",39,21
+                "desktop","United States",3,1
+                "mobile","Brazil",1,0
+                "#
+            ).to_string()
+        )
+    }
 }
